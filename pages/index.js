afd24d38c83ed5b3f9ff7a3c13c8e4e267a1a819
LOKII840 <88399988+LOKII840@users.noreply.github.com>
2022-02-22 22:57:07 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,5 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
+ReactDOM.render(<App />, document.getElementById("root"));
